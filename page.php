2c9e9bfa2fec78bf06df0405611c7e064becbea1
Ivan Panarusky <panaruskyi@gmail.com>
2015-01-18 20:59:36 -0500

    don't show title on home page

--- page.php
+++ page.php
@@ -6,7 +6,7 @@
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<header class="entry-header">
-						<h1 class="entry-title"><?php the_title(); ?></h1>
+						<h1 class="entry-title"><?php if ( ! is_front_page() ) { the_title(); } ?></h1>
 					</header>
 					<div class="entry-content">
 
