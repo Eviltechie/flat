b5be5fa92394cd0da8db1704462e2d898c2b7a6e
Ivan Panarusky <panaruskyi@gmail.com>
2015-02-10 21:15:09 -0500

    don't show title on home page

--- dist/page.php
+++ dist/page.php
@@ -3,7 +3,7 @@
 			<?php while ( have_posts() ) : the_post(); ?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<header class="entry-header">
-						<h1 class="entry-title"><?php the_title(); ?></h1>
+						<h1 class="entry-title"><?php if ( ! is_front_page() ) { the_title(); } ?></h1>
 					</header>
 					<div class="entry-content">
 						<?php the_content( __( 'Continue reading <span class="meta-nav">...</span>', 'flat' ) ); ?>
